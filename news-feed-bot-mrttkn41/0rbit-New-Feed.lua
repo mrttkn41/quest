3a0f4c9c970444b9f8354c04430f5e845b16f526
Mrttkn41 <98215333+mrttkn41@users.noreply.github.com>
2024-05-19 22:41:20 +0300

    Update 0rbit-New-Feed.lua

--- news-feed-bot-mrttkn41/0rbit-New-Feed.lua
+++ news-feed-bot-mrttkn41/0rbit-New-Feed.lua
@@ -1,4 +1,4 @@
--- ao.id  vfDLYiFrxxebGLLDizN8ptRaG5k1XCVOUgvXkJ5Fzm4
+-- ao.id vfDLYiFrxxebGLLDizN8ptRaG5k1XCVOUgvXkJ5Fzm4
 
 local json = require("json")
 local _0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
